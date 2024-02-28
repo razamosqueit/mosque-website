a73a9bbb42b01fb65dd460798497f6f7ce8f0740
tabraiz.chel <tabraiz.chel@matillion.com>
2024-02-28 01:28:48 +0000

    test reading static file

--- app/about-us/history/page.tsx
+++ app/about-us/history/page.tsx
@@ -1,10 +1,13 @@
 import { Text } from '@mantine/core';
 import { BaseLayout } from '@/components/BaseLayout/BaseLayout';
+import { readFile } from '@/lib/read-file';
 
 export default function page() {
+    const content = readFile(`${process.cwd()}/public/test.txt`);
     return (
         <>
             <BaseLayout>
+            <Text>{content}</Text>
             <Text>Historyyyyyy</Text>
             </BaseLayout>
         </>
